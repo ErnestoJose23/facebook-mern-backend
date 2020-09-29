f5c9fdc1c2eb4006382d8912e0ba6a403d20e38e
Ernesto Custodio <ernestojosecg@gmail.com>
2020-09-29 10:35:24 +0200

    Upload

--- models/feedModel.js
+++ models/feedModel.js
@@ -4,7 +4,7 @@ const feedSchema = new mongoose.Schema({
   user_id: { type: String, required: true },
   displayName: { type: String },
   title: { type: String },
-  file: { type: String },
+  imagename: { type: String },
 });
 
 module.exports = Feed = mongoose.model("feed", feedSchema);
--- routes/feedRouter.js
+++ routes/feedRouter.js
@@ -3,50 +3,62 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const auth = require("../middleware/auth");
+const path = require("path");
 const Feed = require("../models/feedModel");
 const File = require("../models/fileModel");
 
 const storage = multer.diskStorage({
-  destination: "./public/",
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
   filename: function (req, file, cb) {
-    cb(null, "IMAGE-" + Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
 const upload = multer({
   storage: storage,
   limits: { fileSize: 1000000 },
-}).single("file");
+});
+
+router.post("/uploadImg", async (req, res) => {
+  const imagen = req.body.imagename;
+  multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, imagen + path.extname(file.originalname));
+    },
+  });
+  console.log(req.body);
+});
 
 router.post("/upload", async (req, res) => {
   try {
-    let { user_id, file, title, displayName } = req.body;
-    if (file != undefined) {
-      upload((req, res) => {
-        const fileSaved = new File();
-        fileSaved.meta_data = req.file;
-        fileSaved.save().then(() => {
-          res.send({ message: "uploaded successfully" });
-        });
-        file = fileSaved;
-        console.log(fileSaved);
-      });
-    }
-
-    console.log(user_id);
+    let { user_id, title, displayName, imagename } = req.body;
     const newFeed = new Feed({
       user_id,
       displayName,
-      file,
+      imagename,
       title,
     });
-    const savedFeed = await newFeed.save();
-    res.json(savedFeed);
+    console.log(newFeed);
+    //const savedFeed = await newFeed.save();
+    //res.json(savedFeed);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
+});
 
-  /*Now do where ever you want to do*/
+router.get("/sync", (req, res) => {
+  Feed.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
 });
 
 module.exports = router;
